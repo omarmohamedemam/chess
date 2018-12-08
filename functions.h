6afe9d279348d81f1dc1a4df06b8e289d0aea737
omarmohamedemam <40744787+omarmohamedemam@users.noreply.github.com>
2018-12-08 20:23:23 +0200

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,45 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+void pr(){
+      //  printf("Welcome To Chess Game\n");
+        printf(" ------ ...... ------ ...... ------ ...... ------ ...... \n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf("|  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |\n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf(" ...... ------ ...... ------ ...... ------ ...... ------ \n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf("|  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |\n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf(" ------ ...... ------ ...... ------ ...... ------ ...... \n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf("|  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |\n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf(" ...... ------ ...... ------ ...... ------ ...... ------ \n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf("|  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |\n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf(" ------ ...... ------ ...... ------ ...... ------ ...... \n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf("|  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |\n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf(" ...... ------ ...... ------ ...... ------ ...... ------ \n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf("|  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |\n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf(" ------ ...... ------ ...... ------ ...... ------ ...... \n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf("|  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |\n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf(" ...... ------ ...... ------ ...... ------ ...... ------ \n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf("|  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |  %c   |\n");
+        printf("|      |      |      |      |      |      |      |      |\n");
+        printf(" ...... ------ ...... ------ ...... ------ ...... ------ \n");
+        printf("There are some instructions yo help you:\n");
+        printf("Type U to undo \t Type R to redo \t Type S to save\n");
+
+}
+
+
+#endif // FUNCTIONS_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+int main()
+{
+    char pieces [9][9];
+    pr();
+    return 0;
+}
